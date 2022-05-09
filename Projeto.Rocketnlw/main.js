89d0398a802d0ad34f7badb1bbf3e7b1ec14a719
Yuri Gabriel <yurigabrielpureza@gmail.com>
2022-05-09 11:31:32 -0300

    Dia-5(Atualização)

--- Projeto.Rocketnlw/main.js
+++ Projeto.Rocketnlw/main.js
@@ -1,10 +1,51 @@
 window.addEventListener('scroll', onScroll)
 
 onScroll()
-
 function onScroll() {
   showNavOnScroll()
   showBackToTopButton()
+
+  activateMenuAtCurrentSection(home)
+  activateMenuAtCurrentSection(services)
+  activateMenuAtCurrentSection(about)
+  activateMenuAtCurrentSection(contact)
+}
+
+function activateMenuAtCurrentSection(section) {
+  // linha alvo
+  const targetLine = scrollY + innerHeight / 2
+
+  // verificar se a secao passou da linha
+  // quais dadps vou precisar?
+
+  // o top da secao
+  const sectionTop = section.offsetTop
+
+  // a altura da secao
+  const sectionHeight = section.offsetHeight
+
+  // o topo da secao chegou ou ultrapassou a linha alvo
+  const sectionTopReachOrPassedTargetline = targetLine >= sectionTop
+
+  // verificar se a base esta abaixo da linha alvo
+  // quais dados vou precisar?
+
+  // a secao termina onde?
+  const sectionEndsAt = sectionTop + sectionHeight
+
+  const sectionEndPassedTargetline = sectionEndsAt <= targetLine
+
+  // limites da secao
+  const sectionBoundaries =
+    sectionTopReachOrPassedTargetline && !sectionEndPassedTargetline
+
+  const sectionId = section.getAttribute('id')
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+  menuElement.classList.remove('active')
+  if (sectionBoundaries) {
+    menuElement.classList.add('active')
+  }
 }
 
 function showNavOnScroll() {
@@ -16,7 +57,7 @@ function showNavOnScroll() {
 }
 
 function showBackToTopButton() {
-  if (scrollY > 500) {
+  if (scrollY > 550) {
     backToTopButton.classList.add('show')
   } else {
     backToTopButton.classList.remove('show')
@@ -35,7 +76,8 @@ ScrollReveal({
   origin: 'top',
   distance: '30px',
   duration: 700
-}).reveal(`#home, 
+}).reveal(`
+#home, 
 #home img, 
 #home .stats, 
 #services,
